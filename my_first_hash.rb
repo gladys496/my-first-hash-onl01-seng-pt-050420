1b76ab8ed53eb05f9dc7623d45a8d842d1feb34b
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-03 05:28:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,8 +19,9 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+    end 
   shipping_manifest["oil paintings"]
-end 
+
 
 
 def adding
